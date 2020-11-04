a2baa73e60964492bcea1e003ce4ed9ae062dada
Cameron Martin <2395521M@student.gla.ac.uk>
2020-11-04 13:28:27 +0000

    Created demo starting point
    I created two scenes, one for the custom building and one for the premade building, I then imported the custom building model and created a basic view in each scene with the desired starting model.

---
+++ Tower Building App/Assets/Camera_Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Camera_Movement : MonoBehaviour{
+    public float speed;
+    public Transform target;
+    
+    // Start is called before the first frame update
+    void Start(){
+        transform.LookAt(target);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.W)){
+            transform.RotateAround(target.position, transform.right, 45 * speed);
+        };
+        if (Input.GetKeyDown(KeyCode.A)){
+            transform.RotateAround(target.position, transform.up, 45 * speed);
+        };
+        if (Input.GetKeyDown(KeyCode.S)){
+            transform.RotateAround(target.position, transform.right, -45 * speed);
+        };
+        if (Input.GetKeyDown(KeyCode.D)){
+            transform.RotateAround(target.position, transform.up, -45 * speed);
+        };
+    }
+}
