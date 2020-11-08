5de141d803c41d48ec4416e9d44cd2c353cc7560
Karthik <dubevedant@gmail.com>
2020-11-08 12:53:03 +0530

    Added jdbc and postgres dependancy. Added Primary User model.

--- src/main/java/com/example/towerbuilderspring/model/User.java
+++ src/main/java/com/example/towerbuilderspring/model/User.java
@@ -1,2 +1,58 @@
-package com.example.towerbuilderspring.model;public class User {
+package com.example.towerbuilderspring.model;
+
+import javax.persistence.*;
+
+public class User {
+
+    @Id // This is the primary key of the database
+    @GeneratedValue(strategy = GenerationType.AUTO) // Creates a new id for each new user via auto increment.
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "towers")
+    private String towers;
+
+    @Column(name = "valid")
+    private boolean  valid;
+
+    public User() {};
+
+    public User(String name, String towers, boolean valid){
+        this.name = name;
+        this.towers = towers;
+        this.valid = valid;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getTowers(){
+        return towers;
+    }
+
+    public boolean getValid(){
+        return valid;
+    }
+
+    public void setTowers(String towers){
+        this.towers= towers;
+    }
+
+    public void setValid(boolean  valid){
+        this.valid = valid;
+    }
+
+    @Override
+    public String toString(){
+        return "Username: " + name + " Has towers: " + towers + " valid user: " + valid + " ";
+    }
+
+
 }
