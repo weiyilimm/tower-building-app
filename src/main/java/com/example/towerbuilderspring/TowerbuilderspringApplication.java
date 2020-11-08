83f6eb8795fdcd6a3e3084d57054f7f091ea72ba
Karthik <dubevedant@gmail.com>
2020-11-08 12:25:29 +0530

    SpringBoot framework initalized

---
+++ src/main/java/com/example/towerbuilderspring/TowerbuilderspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.towerbuilderspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TowerbuilderspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TowerbuilderspringApplication.class, args);
+    }
+
+}
