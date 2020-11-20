6c64bf82cda8a947707ab873255595bb36a6727e
weiyilimm <lwy_1011@hotmail.com>
2020-11-20 20:46:56 +0800

    UI merge with camera movement

---
+++ Tower Building App/Assets/Scripts/UI/LeaderBoardUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LeaderBoardUI : MonoBehaviour
+{
+    public TMP_InputField name;
+    public TMP_InputField score;
+
+    public LeaderBoard leaderBoard;
+
+    void Start()
+    {
+        leaderBoard.Load();
+        name.text = leaderBoard.data.name;
+        score.text = leaderBoard.data.score.ToString();
+    }
+
+
+
+    public void Submit(string name, int score){
+        leaderBoard.data.name = name;
+        leaderBoard.data.score = score;
+        leaderBoard.Save();
+    }
+}
