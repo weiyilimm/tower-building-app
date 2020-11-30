b61ebafbcc9b0995c49b21d06682bcd7807c098c
weiyilimm <lwy_1011@hotmail.com>
2020-11-30 21:15:41 +0800

    Reimplement the icons

---
+++ Tower Building App/Assets/Scripts/UI/ChangeClickImage.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class ChangeClickImage : MonoBehaviour
+{
+    public Sprite normalImage;
+    public Sprite clickedImage;
+    public Button icon;
+    private bool isRotate = true;
+    void Start()
+    {
+        icon = GetComponent<Button>();
+    }
+
+    public void RotationIcon()
+    {
+        if (isRotate == true){
+            icon.image.overrideSprite = clickedImage;
+            isRotate = false;
+        }
+        else{
+            icon.image.overrideSprite = normalImage;
+            isRotate = true;
+        }
+    }
+
+    public void Building()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1){
+            icon.image.overrideSprite = clickedImage;
+        }
+        else{
+            SceneManager.LoadScene(1);
+            icon.image.overrideSprite = clickedImage;
+        }
+    }
+    public void TimingClock()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 2){
+            icon.image.overrideSprite = clickedImage;
+        }
+        else{
+            SceneManager.LoadScene(2);
+            icon.image.overrideSprite = clickedImage;
+        }
+    }
+    public void FriendList()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 3){
+            icon.image.overrideSprite = clickedImage;
+        }
+        else{
+            SceneManager.LoadScene(3);
+            icon.image.overrideSprite = clickedImage;
+        }
+    }
+    public void LeaderBoard()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 4){
+            icon.image.overrideSprite = clickedImage;
+        }
+        else{
+            SceneManager.LoadScene(4);
+            icon.image.overrideSprite = clickedImage;
+        }
+    }
+
+}
+
+
