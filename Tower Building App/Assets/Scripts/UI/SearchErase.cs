6bfc84fb3d7e6c749832a52674e304fc5639c7ba
weiyilimm <lwy_1011@hotmail.com>
2021-01-02 23:32:25 +0800

    Working on #59, created search bar and scene transitions between customisation buildings

---
+++ Tower Building App/Assets/Scripts/UI/SearchErase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SearchErase : MonoBehaviour
+{   
+
+    public TextMeshProUGUI text;
+    public TMP_InputField erase;
+
+    public void Erase(){
+        erase.text = "";
+    }
+
+}
