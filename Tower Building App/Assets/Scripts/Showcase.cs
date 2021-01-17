1df26a3203b8c9034cd0ac077d9d15bc3c6cefc7
Cameron Martin <2395521M@student.gla.ac.uk>
2021-01-17 18:14:31 +0000

    Fixed script for the coming model update
    Changed the script for looping through the matte colours so that it will work with the models once they have been updated to single structures

--- Tower Building App/Assets/Scripts/Showcase.cs
+++ Tower Building App/Assets/Scripts/Showcase.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Showcase : MonoBehaviour
-{
+public class Showcase : MonoBehaviour{
     // Start is called before the first frame update
     void Start(){
         StartCoroutine(materials_showcase());
@@ -16,31 +15,13 @@ public class Showcase : MonoBehaviour
             yield return new WaitForSeconds(2);
             prim_counter += 1;
             sec_counter += 1;
+            
             if (prim_counter == 14) {prim_counter = 0;}
             if (sec_counter == 14) {sec_counter = 0;}
-            foreach (Transform child in transform){
-                int length  = child.GetComponent<Renderer>().materials.Length;
-                if (length == 1){
-                    string mat_name = child.GetComponent<MeshRenderer>().material.name;
-                    if (mat_name.Substring(0,1) == "0" || mat_name.Substring(0,1) == "1"){
-                        child.GetComponent<MeshRenderer>().material = CodeConverter.codes.materials_map[prim_counter];
-                    } else if (mat_name.Substring(0,1) == "2"){
-                        child.GetComponent<MeshRenderer>().material = CodeConverter.codes.materials_map[sec_counter];
-                    }
-                }else{
-                    Material[] mats = child.GetComponent<Renderer>().materials;
-                    string mat1_name = mats[0].name;
-                    string mat2_name = mats[1].name;
-                    if (mat1_name.Substring(0,1) == "0" || mat1_name.Substring(0,1) == "1" || mat1_name.Substring(0,1) == "2"){
-                        mats[0] = CodeConverter.codes.materials_map[sec_counter];
-                    }
-                    //mats[1] = CodeConverter.codes.materials_map[counter];
-                    if (mat2_name.Substring(0,1) == "0" || mat2_name.Substring(0,1) == "1" || mat2_name.Substring(0,1) == "2"){
-                        mats[1] = CodeConverter.codes.materials_map[prim_counter];
-                    }
-                    child.GetComponent<Renderer>().materials = mats;
-                }
-            }
+            Material[] mats = GetComponent<Renderer>().materials;
+            mats[0] = CodeConverter.codes.materials_map[prim_counter];
+            mats[1] = CodeConverter.codes.materials_map[sec_counter];
+            GetComponent<Renderer>().materials = mats;
         }
     }
 
