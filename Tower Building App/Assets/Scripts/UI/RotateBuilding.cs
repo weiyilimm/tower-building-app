ce5c1ebdefe044f153f21f340ce74b26dd61a72a
weiyilimm <lwy_1011@hotmail.com>
2021-01-20 23:04:12 +0800

    Working on #69, created building auto rotation temporarily

--- Tower Building App/Assets/Scripts/UI/RotateBuilding.cs
+++ Tower Building App/Assets/Scripts/UI/RotateBuilding.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,30 +7,28 @@ public class RotateBuilding : MonoBehaviour
 
     [SerializeField] float rotationSpeed = 100f;
     private Touch touch;
-    Rect rect = new Rect(0, 68, 400, 200);
+    Rect rect = new Rect(0, 10, 1000, 1000);
 
     private void Update()
     {   
-        if (Input.touchCount > 0)
-        {   
+        // if (Input.touchCount > 0)
+        // {   
 
-            touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Moved){
-                if (rect.Contains(touch.position)){
-                    transform.Rotate(0f, 0f, -touch.deltaPosition.x * rotationSpeed);
-                }
-            }
-        }
+        //     touch = Input.GetTouch(0);
+        //     if (touch.phase == TouchPhase.Moved){
+        //         if (rect.Contains(touch.position)){
+        transform.Rotate(0f, 0f,  rotationSpeed);
+        //         }
+        //     }
+        // }
 
     }
 
     //method to see the limit area of touch rotating
 
-    public void OnGUI()
-    {
-        GUI.Box(rect, "This is a box");
-    }
+    // public void OnGUI()
+    // {
+    //     GUI.Box (rect,"This is a box covering the player");
+    // }
 }
-
-
 
