b5bec0a952d4f2168d82b6fd1e1f66b7a45ebf1c
Cameron Martin <2395521M@student.gla.ac.uk>
2021-01-20 00:28:36 +0000

    Working on #72 - Updating Scripts
    Created a new script for customising the models next commit will include changing between models. Changes had to be made to CodeConverter such as updating the dictionary orders and indexes of the buildings in the lookup list

---
+++ Tower Building App/Assets/Scripts/Load_Customisation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load_Customisation : MonoBehaviour{
+    // Start is called before the first frame update
+    void Start(){
+        int index = CodeConverter.codes.subject_map[transform.name];
+
+        foreach (Transform child in transform){
+                Material[] mats = child.GetComponent<Renderer>().materials;
+                int primary = User_Data.data.building_stats[index].primary_colour;
+                int secondary = User_Data.data.building_stats[index].secondary_colour;
+                if (primary != 0){
+                    mats[0] = CodeConverter.codes.materials_map[User_Data.data.building_stats[index].primary_colour];
+                }
+                if (secondary != 0){
+                    mats[1] = CodeConverter.codes.materials_map[User_Data.data.building_stats[index].secondary_colour];
+                }
+                child.GetComponent<Renderer>().materials = mats;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
