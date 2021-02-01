39629f03058b83d99030dbfdbd46307a965f3639
Vedant Rakesh Dube <dubevedant@gmail.com>
2021-02-01 14:10:07 +0530

    Working on #74. Added user building validator service.
    Created a service that checks if the building and the user requested are
    valid. It then checks if the user has that building already set.

--- backend/src/main/java/com/example/towerbuilderspring/repository/UserRepository.java
+++ backend/src/main/java/com/example/towerbuilderspring/repository/UserRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<Users, UUID> {
     List<Users> findByEmail(String email);
+
 }
