bd1db4c1c368818e706c039ce120474d2dc8f08a
Wei Yi Lim <2474554l@student.gla.ac.uk>
2021-02-04 00:59:35 +0800

    Working on #83, created local xp in Clock Scene

--- Tower Building App/Assets/Scripts/UI/StopWatch.cs
+++ Tower Building App/Assets/Scripts/UI/StopWatch.cs
@@ -1,31 +1,27 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class StopWatch : MonoBehaviour {
 
-    private TextMeshProUGUI text;
-    float theTime;
-    public float speed = 1;
-    bool playing;
+    public TextMeshProUGUI Timer;
+    public TextMeshProUGUI Score;
+    public int MultiplierXP = 2;
+    private bool playing;
+    private float theTime;
 
- // Use this for initialization
-    void Start () {
-        
-        text = GetComponent<TMPro.TextMeshProUGUI>();
-    }
  
- // Update is called once per frame
     void Update () {
         if (playing == true)
         {
-            theTime += Time.deltaTime * speed;
+            theTime += Time.deltaTime;
             string hours = Mathf.Floor((theTime % 216000) / 3600).ToString("00");
             string minutes = Mathf.Floor((theTime % 3600) / 60).ToString("00");
             string seconds = (theTime % 60).ToString("00");
-            text.text = hours + ":" + minutes + ":" + seconds;
+            Timer.text = hours + ":" + minutes + ":" + seconds;
         }
     }
 
@@ -40,9 +36,10 @@ public class StopWatch : MonoBehaviour {
     }
 
     public void Reset()
-    {
+    {   
+        Score.text = (Math.Round(theTime) * MultiplierXP).ToString() + "XP ";
         playing = false;
-        text.text = "00:00:00";
+        Timer.text = "00:00:00";
         theTime = 0;
     }
 }
