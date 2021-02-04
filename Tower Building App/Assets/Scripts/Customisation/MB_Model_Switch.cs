0a8cb27582f3fc2f60bf7236f5650aadf14f05c3
Cameron Martin <2395521M@student.gla.ac.uk>
2021-02-04 19:35:05 +0000

    New script setup and audio listener tidy
    Created two new scripts that will be added to the customisation once the new UI is added, and removed the excess audio listeners from each of the customisation scenes

---
+++ Tower Building App/Assets/Scripts/Customisation/MB_Model_Switch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class MB_Model_Switch : MonoBehaviour{
+    public GameObject parent;
+    void Update(){
+        foreach (Transform child in parent.transform){
+            int child_length = child.transform.childCount;
+            for(int counter=0; counter<child_length; counter++){
+                GameObject grandchild = parent.transform.GetChild(counter).gameObject;
+                if (grandchild.activeSelf) {
+                    User_Data.data.temp_model = counter;
+                }
+            }
+        }
+    }    
+}
