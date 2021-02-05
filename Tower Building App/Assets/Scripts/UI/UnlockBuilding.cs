d4c46109c1a44993d5b96bafd916ccf0f2d9133d
weiyilimm <lwy_1011@hotmail.com>
2021-02-05 23:21:06 +0800

    Working on #83, implemented local xp and global xp in Biology Scene. User can now earn XP from timer and unlock materials and buildings

--- Tower Building App/Assets/Scripts/UI/UnlockBuilding.cs
+++ Tower Building App/Assets/Scripts/UI/UnlockBuilding.cs
@@ -2,18 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UnlockBuilding : MonoBehaviour
 {
     public GameObject[] buildings;
-    private double XP;
+    private double globalXP;
+    private double localXP;
     private Button button;
     private int BuildingXP = 2000;
     private GameObject lockIcon;
-
+    private string currentSceneName;
     void Start()
     {   
-        XP = StopWatch.GlobalXP;
+        currentSceneName = SceneManager.GetActiveScene().name;
+        switch (currentSceneName){
+            case "Main":
+                localXP = StopWatch.MainXP;
+                break;
+            case "Arts":
+                localXP = StopWatch.ArtsXP;
+                break;
+            case "BioChe":
+                localXP = StopWatch.BioCheXP;
+                break;
+            case "ComSci":
+                localXP = StopWatch.ComSciXP;
+                break;
+            case "Eng":
+                localXP = StopWatch.EngXP;
+                break;
+            case "Geo":
+                localXP = StopWatch.GeoXP;
+                break;
+            case "Lan":
+                localXP = StopWatch.LanXP;
+                break;
+            case "LawPol":
+                localXP = StopWatch.LawPolXP;
+                break;
+            case "PhyMath":
+                localXP = StopWatch.PhyMathXP;
+                break;
+        }
+        globalXP = StopWatch.GlobalXP;
         button = this.GetComponent<Button>();
         lockIcon = this.transform.GetChild(0).gameObject;
         for (int i = 0; i<buildings.Length; i++){
@@ -23,7 +55,7 @@ public class UnlockBuilding : MonoBehaviour
             Second building = 500
             Third building = 1000
             */
-            if (XP >= i * BuildingXP){
+            if ((globalXP >= i * BuildingXP) || (localXP >= i * BuildingXP)){
                 if (button.name[0].ToString() == i.ToString()){
                     lockIcon.SetActive(false);
                     for (int j = 0; j<buildings.Length; j++){
