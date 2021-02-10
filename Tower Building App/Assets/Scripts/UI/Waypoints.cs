72cf8e44a3fa19d1d7545beb1721fddf9acbedf8
Joe Subbiani <2377990s@student.gla.ac.uk>
2021-02-10 13:45:39 +0000

    Working on #67 Created a Waypoint marker for buildings
    Building subject title is displayed on building podium. 1 line script attached to some UI elements. If it is to be a feature it should be refined, maybe only show the waypoint within a certain proximity

---
+++ Tower Building App/Assets/Scripts/UI/Waypoints.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Waypoints : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Camera.main.WorldToScreenPoint(target.position);
+    }
+}
