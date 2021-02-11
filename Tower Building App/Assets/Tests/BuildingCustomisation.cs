37f6c015816a0435604ccbd55cff6e0b29f87f76
Joe Subbiani <2377990s@student.gla.ac.uk>
2021-02-11 13:21:25 +0000

    Working on #91 Planning out test files
    Nothing complicated, just setting up some test files so me and Reuben can work on some tests simultanously

--- Tower Building App/Assets/Tests/BuildingCustomisation.cs
+++ Tower Building App/Assets/Tests/BuildingCustomisation.cs
@@ -6,15 +6,32 @@ using UnityEngine.TestTools;
 
 namespace Tests
 {
-    public class NewTestScript
+    // Roy Osherove's naming strategy
+    // MethodName_StateUnderTest_ExpectedBehavior
+
+    public class BuildingCustomisation
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void NewTestScriptSimplePasses()
+        public void ExpectedMaterials_MaterialCallGetsMaterial_True()
         {
-            // Use the Assert class to test conditions
+            //get material
+            //is material expected material
+
+            //matte white
+            Assert.IsTrue(true);
+
+            //matte red
+            Assert.IsTrue(true);
         }
 
+        [Test]
+        public void ExpectedBuilding_ModelIDGetsModel_True()
+        {
+            Assert.IsTrue(true);
+        }
+
+        /*
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
@@ -24,5 +41,6 @@ namespace Tests
             // Use yield to skip a frame.
             yield return null;
         }
+        */
     }
 }
---
+++ Tower Building App/Assets/Tests/XPMethods.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    // Roy Osherove's naming strategy
+    // MethodName_StateUnderTest_ExpectedBehavior
+
+    public class XPMethods
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TimerXP_TimeReturnsCorrectXP_TimeEqualsXP()
+        {
+            //"Run timer" - set timer value
+            //get XP for this value
+            //test if equivilant
+            int time;
+
+            //Seconds
+            time = 22;
+            Assert.AreEqual(time, 22);
+
+            //Minutes
+            time = 458;
+            Assert.AreEqual(time, 458);
+
+            //Hour
+            time = 3600;
+            Assert.AreEqual(time, 3600);
+        }
+
+        [Test]
+        public void TimerDisplay_TimerHourMinuteSecondMatchesSecondsConversion_TimeEqualsExpectedTimeSeconds()
+        {
+            int time;
+
+            //Seconds
+            time = 22;
+            Assert.AreEqual(time, 22);
+
+            //Minutes
+            time = 458;
+            Assert.AreEqual(time, 458);
+
+            //Hour
+            time = 3600;
+            Assert.AreEqual(time, 3600);
+        }
+
+
+        /*
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator XPMethodsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+        */
+    }
+}
