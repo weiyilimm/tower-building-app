c62c292f64e34695267de80f54bc6729d39ff7fc
Vedant Rakesh Dube <dubevedant@gmail.com>
2021-02-17 17:13:12 +0530

    Working on #101. Moved Json formatting to UserController class.
    The changed the GET request for the user buildings to now format the
    data as it is modelled in unity.

--- backend/src/main/java/com/example/towerbuilderspring/model/UserModelId.java
+++ backend/src/main/java/com/example/towerbuilderspring/model/UserModelId.java
@@ -21,4 +21,44 @@ public class UserModelId implements Serializable {
 
     public UserModelId() {};
 
+    @Override
+    public String toString() {
+        return "UserModelId{" +
+                "fk_user=" + fk_user +
+                ", fk=" + fk +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserModelId)) return false;
+        UserModelId that = (UserModelId) o;
+        return fk_user.equals(that.fk_user) &&
+                Objects.equals(fk, that.fk);
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+
+    }@Override
+    public int hashCode() {
+        return Objects.hash(fk_user, fk);
+    }
+
+    public Users getFk_user() {
+        return fk_user;
+    }
+
+    public void setFk_user(Users fk_user) {
+        this.fk_user = fk_user;
+    }
+
+    public BuildingModels getFk_building() {
+        return fk;
+    }
+
+    public void setFk_building(BuildingModels fk) {
+        this.fk = fk;
+    }
 }
--- backend/src/main/java/com/example/towerbuilderspring/model/UserModels.java
+++ backend/src/main/java/com/example/towerbuilderspring/model/UserModels.java
@@ -36,7 +36,65 @@ public class UserModels {
         this.modelGroup = modelGroup;
     }
 
+    @Override
+    public String toString() {
+        return "UserModels{" +
+                "userModelId=" + userModelId.toString() +
+                ", building_xp=" + building_xp +
+                ", height=" + height +
+                ", primaryColour=" + primaryColour +
+                ", secondaryColour=" + secondaryColour +
+                ", modelGroup=" + modelGroup +
+                '}';
+    }
 
+    public UserModelId getUserModelId() {
+        return userModelId;
+    }
+
+    public void setUserModelId(UserModelId userModelId) {
+        this.userModelId = userModelId;
+    }
+
+    public int getBuilding_xp() {
+        return building_xp;
+    }
+
+    public void setBuilding_xp(int building_xp) {
+        this.building_xp = building_xp;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getPrimaryColour() {
+        return primaryColour;
+    }
+
+    public void setPrimaryColour(int primaryColour) {
+        this.primaryColour = primaryColour;
+    }
+
+    public int getSecondaryColour() {
+        return secondaryColour;
+    }
+
+    public void setSecondaryColour(int secondaryColour) {
+        this.secondaryColour = secondaryColour;
+    }
+
+    public int getModelGroup() {
+        return modelGroup;
+    }
+
+    public void setModelGroup(int modelGroup) {
+        this.modelGroup = modelGroup;
+    }
     //    @Embeddable
 //    public static class userModelId implements Serializable {
 //        private static final long serialVersionUID = 1l;
