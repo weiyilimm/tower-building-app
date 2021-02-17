68340a8747da2e6a4d34ed7b52720f555ac2efa9
Wei Yi Lim <2474554l@student.gla.ac.uk>
2021-02-18 00:54:03 +0800

    Working on #100, implemented pop up box when the user not connected to internet

---
+++ Tower Building App/Assets/Scripts/UI/InternetFailure.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InternetFailure : MonoBehaviour
+{   
+    public GameObject PopUpInternetFailure;
+    public GameObject StartButton;
+    // Start is called before the first frame update
+    void Update()
+    {
+        StartCoroutine(checkInternet((isNotConnected) => 
+        {
+            if(isNotConnected){
+                PopUpInternetFailure.SetActive(true);
+                StartButton.SetActive(false);
+            }
+            else{
+                PopUpInternetFailure.SetActive(false);
+                StartButton.SetActive(true);
+            }
+        }));
+    }
+
+    IEnumerator checkInternet(Action<bool> action){
+        WWW www = new WWW("https://www.google.com/");
+        yield return www;
+        if (www.error != null){
+            action(true);
+        }
+        else{
+            action(false);
+        }
+    }
+}
