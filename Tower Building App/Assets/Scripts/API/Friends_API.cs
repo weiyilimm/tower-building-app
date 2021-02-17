72e7ba2d89338504df42290d04d9e6a4656bc257
Cameron Martin <2395521M@student.gla.ac.uk>
2021-02-17 20:45:05 +0000

    Working on #93 - Setup
    Started work on the two scripts that will handle the API requests for the leaderboard and the friends list. Created a first draft of the JSON structure for each in their respective files

---
+++ Tower Building App/Assets/Scripts/API/Friends_API.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Friends_API : MonoBehaviour {
+    
+    List<Friends> friendslist = new List<Friends>();
+
+    /*  JSON formatting - NOT YET FINAL!
+        {"user":"uibivd,
+         "friends": [
+            {"id":cbjsfd, "userName":"BobertRoss"},
+            ...
+            {"id":jdyrit, "userName":"JumJumJr"}
+        ]} 
+    */
+
+    void Start() {
+        // GET request - get the friends list of the current User
+
+        // Translate the data retrieved from the GET request
+
+        // Display the data using the UI
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
+
+public class Friends {
+    string UserId;
+    string UserName;
+
+    public Friends(string ui, string un) {
+        UserId = ui;
+        UserName = un;
+    }
+}
---
+++ Tower Building App/Assets/Scripts/API/Leaderboard_API.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaderboard_API : MonoBehaviour {
+    
+    public List<leaderboard_data> LB_data = new List<leaderboard_data>();
+    
+    /*  JSON formatting - NOT YET FINAL!
+        {"users": [
+            {"id":sgisvi, "userName":"BobertRoss", totalExp: 25000},
+            {"id":fsiufb, "userName":"RobertBoss", totalExp: 16000},
+            {"id":oncsbd, "userName":"JimJimSr", totalExp: 12000},
+            {"id":mbcuse, "userName":"JumJumJr", totalExp: 5000},
+            ...
+            {"id":sgisvi, "userName":"user1", totalExp: 1000},
+        ]}
+    */
+
+    void Start() {
+        // GET Request - Top 50 users by totalExp
+        // CreateRequest("GET_Leaderboard");
+
+        // Translate the data retrieved from the GET request
+
+        // Display the data using the UI
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
+
+public class leaderboard_data {
+    public string UserId;
+    public string UserName;
+    public int TotalExp;
+
+    public leaderboard_data(string ui, string un, int xp) {
+        UserId = ui;
+        UserName = un;
+        TotalExp = xp;
+    }
+}
