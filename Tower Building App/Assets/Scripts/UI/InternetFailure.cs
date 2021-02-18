c363fca387b5e980e46109601b41e18a453893b7
Wei Yi Lim <2474554l@student.gla.ac.uk>
2021-02-19 03:45:07 +0800

    Working on #100, fixed cant enter main scene bug. Implemented a loading text to show user as the test connection will take some time

--- Tower Building App/Assets/Scripts/UI/InternetFailure.cs
+++ Tower Building App/Assets/Scripts/UI/InternetFailure.cs
@@ -10,35 +10,35 @@ public class InternetFailure : MonoBehaviour
 {   
     public GameObject PopUpInternetFailure;
     public GameObject StartGameObject;
+    public GameObject LoadingText;
     public Button StartButton;
     public Button PopUpInternetButton;
-    private bool isConnected;
+    private bool isConnected = false;
     // Start is called before the first frame update
     void Start()
     {
         StartButton.onClick.AddListener(() => StartCoroutine(checkInternet()));
-        if (isConnected){
-             StartButton.onClick.AddListener(() => SceneManager.LoadScene(1));
-        }
-        else{
+        if (!isConnected){
             PopUpInternetButton.onClick.AddListener(() => SceneManager.LoadScene(0));
         }
     }
 
     IEnumerator checkInternet(){
+        StartGameObject.SetActive(false);
+        LoadingText.SetActive(true);
         UnityWebRequest request = new UnityWebRequest ("http://google.com");
         yield return request.SendWebRequest();
         //Is not connected
         if (request.error != null){
             PopUpInternetFailure.SetActive(true);
-            StartGameObject.SetActive(false);
+            // StartGameObject.SetActive(false);
+            LoadingText.SetActive(false);
             isConnected = false;
         }
         //Is connected
         else{
-            PopUpInternetFailure.SetActive(false);
-            StartGameObject.SetActive(true);
             isConnected = true;
+            SceneManager.LoadScene(1);
         }
     }
 
