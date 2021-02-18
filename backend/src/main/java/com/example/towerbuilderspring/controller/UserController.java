8b5bfbd3f762ef9509a3d05b0c707229a306a086
Vedant Rakesh Dube <dubevedant@gmail.com>
2021-02-19 02:00:02 +0530

    Working on #101. Finshed post functionaly for individual user buildings.
    The post request can now add new buildings and update them
    automatically.
    A full functionality test suite is required however.

--- backend/src/main/java/com/example/towerbuilderspring/controller/UserController.java
+++ backend/src/main/java/com/example/towerbuilderspring/controller/UserController.java
@@ -8,11 +8,16 @@ import com.example.towerbuilderspring.repository.ModelRepository;
 import com.example.towerbuilderspring.repository.UserModelRepository;
 import com.example.towerbuilderspring.repository.UserRepository;
 import com.example.towerbuilderspring.service.BuildingRequestValid;
-import org.aspectj.asm.IModelFilter;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import java.lang.reflect.Array;
 import java.util.*;
@@ -191,57 +196,81 @@ public class UserController {
      within the user repository rather then a user (which would be directly modifiable through the
      repository interface) itself.
     **/
-//    @PostMapping("/Users/{userId}/Buildings/{buildingId}")
-//    public ResponseEntity<Set<BuildingModels>> updateUserBuilding(@PathVariable("userId") UUID userId,
-//                                                           @PathVariable("buildingId") long buildingId,
-//                                                           @RequestBody UserModels newUserModel) {
-//
-//        Optional<Users> fetched_user = userRepository.findById(userId);
-//        Optional<BuildingModels> fetched_model = modelRepository.findById(buildingId);
-//        List<UserModels> all_models = userModelRepository.findAll();
-//
-//        // Make sure that both the user and the building exist.
-//        if (fetched_user.isPresent() && fetched_model.isPresent()) {
-//
-//            System.out.println("Step1");
-//            System.out.println();
-//
-//            Users user = fetched_user.get();
-//            BuildingModels newModel = fetched_model.get();
-//            // Get the group to be updated.
-//            long modelGroup = newModel.getModelGroup();
-//
-//            // Search variables
-//            UserModels currentModel;
-//            Users currentUser;
-//            BuildingModels currentBuilding;
-//            // TODO See if you can make a method in the User Models Repository to do this instead.
-//            // Go through all the global models and find the one belonging both to the same group and the same user.
-//            for (int i = 0; i < all_models.size(); i++) {
-//                currentModel = all_models.get(i);
-//                currentUser = currentModel.getUserModelId().getFk_user();
-//                currentBuilding = currentModel.getUserModelId().getFk_building();
-//
-//                System.out.print(currentBuilding);
-//                System.out.println(" " + modelGroup);
-//
-//                System.out.print(currentUser);
-//                System.out.println(" " + userId.toString());
-//
-//                System.out.println();
-//
-//                if (currentUser.getId() == userId &&
-//                        currentBuilding.getModelGroup() == modelGroup) {
-//                    System.out.println("deleting a model");
-//                    UserModelId modelToDelete = new UserModelId(currentUser, currentBuilding);
-////                    userModelRepository.deleteById(modelToDelete);
-//                    userModelRepository.save(newUserModel);
-//                    return new ResponseEntity<>(null, HttpStatus.OK);
-//                }
-//            }
-//        }
-//        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-//    }
+    @PostMapping("/Users/{userId}/Buildings/{buildingId}")
+    public ResponseEntity<UserModels> updateUserBuilding(@PathVariable("userId") UUID userId,
+                                                           @PathVariable("buildingId") long buildingId,
+                                                           @RequestBody String newUserModel) throws ParseException {
+
+        Optional<Users> fetched_user = userRepository.findById(userId);
+        Optional<BuildingModels> fetched_model = modelRepository.findById(buildingId);
+        List<UserModels> all_models = userModelRepository.findAll();
+
+        UserModels modelToAdd = new UserModels();
+        UserModelId modelId = new UserModelId();
+
+        // Make sure the model id given is matches the model id in the body.
+        if (buildingId != fetched_model.get().getBuildingCode()) {
+            return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+        }
+
+        // Make sure that both the user and the building exist.
+        if (fetched_user.isPresent() && fetched_model.isPresent()) {
+
+            // Manually parsing Json.
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(newUserModel);
+
+            // Create the Composite key
+            modelId.setFk_building(fetched_model.get());
+            modelId.setFk_user(fetched_user.get());
+
+            modelToAdd.setUserModelId(modelId);
+
+            // Set the rest of the attributes.
+            modelToAdd.setBuilding_xp((((Long) json.get("building_xp")).intValue()));
+            modelToAdd.setHeight((((Long) json.get("height")).intValue()));
+            modelToAdd.setModelGroup((Long) json.get("modelGroup"));
+            modelToAdd.setPrimaryColour((((Long) json.get("primaryColour")).intValue()));
+            modelToAdd.setSecondaryColour((((Long) json.get("secondaryColour")).intValue()));
+
+            Users user = fetched_user.get();
+            BuildingModels newModel = fetched_model.get();
+            // Get the group to be updated.
+            long modelGroup = modelToAdd.getModelGroup();
+
+            System.out.println("search started");
+
+            // Search variables
+            UserModels currentModel;
+            Users currentUser;
+            BuildingModels currentBuilding;
+            // TODO See if you can make a method in the User Models Repository to do this instead.
+            // Go through all the global models and find the one belonging both to the same group and the same user.
+            for (int i = 0; i < all_models.size(); i++) {
+                currentModel = all_models.get(i);
+                currentUser = currentModel.getUserModelId().getFk_user();
+                currentBuilding = currentModel.getUserModelId().getFk_building();
+
+
+                if (currentUser.getId() == userId &&
+                        currentBuilding.getModelGroup() == modelGroup) {
+                    System.out.println();
+                    UserModelId modelToDelete = new UserModelId(currentUser, currentBuilding);
+                    System.out.println(modelToAdd.toString());
+                    System.out.println();
+
+                    userModelRepository.deleteById(modelToDelete);
+                    userModelRepository.save(modelToAdd);
+
+                    return new ResponseEntity<>(modelToAdd, HttpStatus.OK);
+                }
+            }
+            // The user doesn't have any models belonging to that group yet (i.e. initialising the user).
+            userModelRepository.save(modelToAdd);
+            return new ResponseEntity<>(modelToAdd, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(modelToAdd, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 //    @PostMapping("/Users/{userId}/Buildings/{buildingId}")
 //    public ResponseEntity<Set<BuildingModels>> changeUserBuilding(@PathVariable("userId") UUID userId,
