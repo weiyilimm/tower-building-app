9df972b5d5c1f3f6c1e39dedcb8fd660441b9639
weiyilimm <lwy_1011@hotmail.com>
2021-02-21 16:39:52 +0800

    Working on #94, implemented update leaderboard in UI

--- Tower Building App/Assets/Scripts/API/Leaderboard_API.cs
+++ Tower Building App/Assets/Scripts/API/Leaderboard_API.cs
@@ -4,12 +4,23 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 using SimpleJSON;
+using UnityEngine.UI;
+using TMPro;
 
 [System.Serializable]
 public class Leaderboard_API : MonoBehaviour {
     
     public List<leaderboard_data> LB_data = new List<leaderboard_data>();
-    
+    //Use the prefab participant
+    public Transform Participant;
+    //Leaderboardlist to be able to store all instances
+    public Transform LeaderBoardList;
+    private GameObject firstTrophy;
+    private GameObject secondTrophy;
+    private GameObject thirdTrophy;
+    private GameObject rankingText;
+    private TextMeshProUGUI textXP;
+    private TextMeshProUGUI textName;
     /*  JSON formatting - NOT YET FINAL!
         {"users": [
             {"id":sgisvi, "userName":"BobertRoss", totalExp: 25000},
@@ -35,8 +46,48 @@ public class Leaderboard_API : MonoBehaviour {
 
         //Print out the data for the five users in the leaderboard
         foreach (leaderboard_data data in LB_data){
+            int index = LB_data.IndexOf(data);
+            firstTrophy = Participant.Find("TrophyFirst").gameObject;
+            secondTrophy = Participant.Find("TrophySecond").gameObject;
+            thirdTrophy = Participant.Find("TrophyThird").gameObject;
+            rankingText = Participant.Find("RankingText").gameObject;
+            if (index == 0){
+                firstTrophy.SetActive(true);
+                secondTrophy.SetActive(false);
+                thirdTrophy.SetActive(false);
+                rankingText.SetActive(false);
+            }
+            else if (index == 1){
+                firstTrophy.SetActive(false);
+                secondTrophy.SetActive(true);
+                thirdTrophy.SetActive(false);
+                rankingText.SetActive(false);
+            }
+            else if (index == 2){
+                firstTrophy.SetActive(false);
+                secondTrophy.SetActive(false);
+                thirdTrophy.SetActive(true);
+                rankingText.SetActive(false);
+            }
+            else{
+                firstTrophy.SetActive(false);
+                secondTrophy.SetActive(false);
+                thirdTrophy.SetActive(false);
+                rankingText.SetActive(true);
+
+            }
+            //Create instance(user) as each data loop
+            var instance = Instantiate(Participant);
+            //Set their parent to leaderboardlist
+            instance.SetParent(LeaderBoardList);
+            textName = instance.Find("NameText").gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+            textXP = instance.Find("XPText").gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+            textName.text = data.UserName;
+            textXP.text = data.TotalExp.ToString();
+            Debug.Log(LB_data.IndexOf(data));
             Debug.Log(data.UserName + " " + data.TotalExp);
         }
+
         
         // Display the data using the UI -> Put this into a coroutine.
 
