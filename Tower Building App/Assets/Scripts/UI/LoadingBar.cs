6005ec0e77b227e6483887b27b9a760938518a99
Wei Yi Lim <2474554l@student.gla.ac.uk>
2021-02-21 19:41:54 +0800

    Working on #92, created better loading bar

---
+++ Tower Building App/Assets/Scripts/UI/LoadingBar.cs
@@ -0,0 +1,24 @@
+﻿// using System.Collections;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using UnityEngine.SceneManagement;
+
+// public class LoadingBar : MonoBehaviour
+// {   
+
+//     public Slider slider;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         StartCoroutine(LoadProgress());
+//     }
+
+//     IEnumerator LoadProgress(){
+//         AsyncOperation operation = SceneManager.LoadScene(1);
+//         while (!operation.isDone){
+//             float progress = Mathf.Clamp01(operation.progess/.9f);
+//             slider.value = progress;
+//             yield return null;
+//         }
+
+//     }
