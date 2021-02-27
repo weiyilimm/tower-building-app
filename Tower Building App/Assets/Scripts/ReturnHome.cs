0d9c6a931a7334d961a77c8f06afad3b2a50c77c
Cameron Martin <2395521M@student.gla.ac.uk>
2021-02-27 16:43:17 +0000

    Working on #92 - Returning to home world
    Created a new script which is just a slightly modified version of the current script to go to the main scene, however this version contains an API call to the database to retrieve their building data to overwrite the friends data that is currently stored there

---
+++ Tower Building App/Assets/Scripts/ReturnHome.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReturnHome : MonoBehaviour {
+    
+    public Sprite normalImage;
+    public Sprite clickedImage;
+    public Button icon;
+
+    public CameraMovePhone movement;
+
+
+    void Start()
+    {
+        icon = GetComponent<Button>();
+    }
+    public void LoadPersonalBuildingData()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1){
+            icon.image.overrideSprite = clickedImage;
+        }
+        else{
+            icon.image.overrideSprite = clickedImage;
+            User_Data.data.CreateRequest("GET_User");
+            SceneManager.LoadScene(1);
+        }
+
+        //plays standard button click sound
+        FindObjectOfType<SoundManager>().Play("standard button click");
+
+        //turn filter off
+        FindObjectOfType<ListenerPersist>().toggleFilterOn(false);
+    }
+}
