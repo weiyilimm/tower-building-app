5e9163334eee721fe450e9a0a319c737bd07f92c
Wei Yi Lim <2474554l@student.gla.ac.uk>
2021-02-28 22:26:18 +0800

    Working on #84, created username is valid feature which detect when the username is already existed

--- Tower Building App/Assets/Scripts/UI/Login.cs
+++ Tower Building App/Assets/Scripts/UI/Login.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Text.RegularExpressions;
+using SimpleJSON;
+using UnityEngine.Networking;
+
 public class Login : MonoBehaviour
 {   
     //Login and register navigation bar
@@ -11,12 +14,15 @@ public class Login : MonoBehaviour
     //Login panel 
     public GameObject LoginPanel;
     public TMP_InputField Email;
+    public TMP_InputField Username;
     public GameObject InvalidUsernamePopUP;
     public GameObject InvalidEmailPopUP;
     public Button RegisterButton;
-    private bool usernameValid = true;
+    public List<leaderboard_data> AllUsers = new List<leaderboard_data>();    
+    private bool usernameIsValid = true;
     void Start()
-    {
+    {   
+        CreateRequest("GET_Users");
         RegisterButton.onClick.AddListener(() => Register());
     }
 
@@ -40,19 +46,21 @@ public class Login : MonoBehaviour
         }
 
     public void Register(){
-        foreach (leaderboard_data data in Leaderboard_API.LB_data_InOrder){
+        foreach (leaderboard_data data in AllUsers){
             //Check if the username already exist
-            if (data.UserName.ToLower() == Email.text.ToLower()){
-                usernameValid = false;
+            if (data.UserName.ToLower() == Username.text.ToLower()){
+                Debug.Log("Username has been taken");
+                usernameIsValid = false;
             }
         }
         //If username can be used
-        if (usernameValid){
+        if (usernameIsValid){
             InvalidUsernamePopUP.SetActive(false);
         }
-        //If username has already taken
+        //If username has been taken
         else{
             InvalidUsernamePopUP.SetActive(true);
+            usernameIsValid = true;
         }
         //If the email syntax is correct
         if(IsEmail(Email.text)){
@@ -61,6 +69,47 @@ public class Login : MonoBehaviour
         //If the emeail syntax is not correct
         else{
             InvalidEmailPopUP.SetActive(true);
+        }
+    }
+
+    void CreateRequest(string RequestType) {
+        string apiString = "https://uni-builder-database.herokuapp.com/api/Users/";
+
+        if (RequestType == "GET_Users"){ 
+            StartCoroutine(GetRequest(apiString));
+        }
+    }
+
+    IEnumerator GetRequest(string targetAPI) {
+        Debug.Log(targetAPI);
+        // Constructs and sends a GET request to the database to retreive a JSON file
+        UnityWebRequest uwr = UnityWebRequest.Get(targetAPI);
+        yield return uwr.SendWebRequest();
+        if (uwr.isNetworkError) {
+            Debug.Log("An Internal Server Error Was Encountered");
+        } else {
+            string raw = uwr.downloadHandler.text;
+            Debug.Log("Received: " + raw);
+
+            // TRANSLATION CODE HERE
+            TranslateToLeaderboard(raw);
+        }
+    }
+
+    private void TranslateToLeaderboard(string rawJSON){ 
+
+        JSONNode node;
+        node = JSON.Parse(rawJSON);
+        string userid;
+        string username;
+        int totalExp;
+        
+        for (int i=0; i<5; i++) {
+            userid = JSON.Parse(node[i]["id"].Value);
+            username = JSON.Parse(node[i]["userName"].Value);
+            totalExp = JSON.Parse(node[i]["totalExp"].Value);
+            leaderboard_data data = new leaderboard_data(userid, username, totalExp);
+            AllUsers.Add(data);
         }
     }
 }
