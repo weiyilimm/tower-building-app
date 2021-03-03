6611dd2970d42b81f1faa711a522451ad5aadee9
Vedant Rakesh Dube <dubevedant@gmail.com>
2021-03-03 13:18:00 +0530

    Working on #110. Moved decoupled code from UserModelDecoupled to the original class

--- backend/src/main/java/com/example/towerbuilderspring/model/UserModelId.java
+++ backend/src/main/java/com/example/towerbuilderspring/model/UserModelId.java
@@ -7,6 +7,8 @@ import java.io.Serializable;
 import java.util.Objects;
 import java.util.UUID;
 
+
+
 @Embeddable
 public class UserModelId implements Serializable {
     private static final long serialVersionUID = 1l;
@@ -15,22 +17,20 @@ public class UserModelId implements Serializable {
     @MapsId
     private Users fk_user;
 
-    @ManyToOne
-    @MapsId("buildingCode")
-    private BuildingModels fk;
+    private long model;
 
     public UserModelId() {};
 
-    public UserModelId(Users user, BuildingModels model) {
+    public UserModelId(Users user, long model) {
         this.fk_user = user;
-        this.fk = model;
+        this.model = model;
     }
 
     @Override
     public String toString() {
         return "UserModelId{" +
                 "fk_user=" + fk_user +
-                ", fk=" + fk +
+                ", model=" + model +
                 '}';
     }
 
@@ -38,17 +38,19 @@ public class UserModelId implements Serializable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof UserModelId)) return false;
-        UserModelId that = (UserModelId) o;
-        return fk_user.equals(that.fk_user) &&
-                Objects.equals(fk, that.fk);
+        UserModelId modelId = (UserModelId) o;
+        return model == modelId.model &&
+                Objects.equals(fk_user, modelId.fk_user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fk_user, model);
     }
 
     public static long getSerialVersionUID() {
         return serialVersionUID;
 
-    }@Override
-    public int hashCode() {
-        return Objects.hash(fk_user, fk);
     }
 
     public Users getFk_user() {
@@ -59,11 +61,73 @@ public class UserModelId implements Serializable {
         this.fk_user = fk_user;
     }
 
-    public BuildingModels getFk_building() {
-        return fk;
+    public long getModel() {
+        return model;
     }
 
-    public void setFk_building(BuildingModels fk) {
-        this.fk = fk;
+    public void setModel(long model) {
+        this.model = model;
     }
 }
+
+
+//@Embeddable
+//public class UserModelId implements Serializable {
+//    private static final long serialVersionUID = 1l;
+//
+//    @ManyToOne
+//    @MapsId
+//    private Users fk_user;
+//
+//    @ManyToOne
+//    @MapsId("buildingCode")
+//    private BuildingModels fk;
+//
+//    public UserModelId() {};
+//
+//    public UserModelId(Users user, BuildingModels model) {
+//        this.fk_user = user;
+//        this.fk = model;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "UserModelId{" +
+//                "fk_user=" + fk_user +
+//                ", fk=" + fk +
+//                '}';
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof UserModelId)) return false;
+//        UserModelId that = (UserModelId) o;
+//        return fk_user.equals(that.fk_user) &&
+//                Objects.equals(fk, that.fk);
+//    }
+//
+//    public static long getSerialVersionUID() {
+//        return serialVersionUID;
+//
+//    }@Override
+//    public int hashCode() {
+//        return Objects.hash(fk_user, fk);
+//    }
+//
+//    public Users getFk_user() {
+//        return fk_user;
+//    }
+//
+//    public void setFk_user(Users fk_user) {
+//        this.fk_user = fk_user;
+//    }
+//
+//    public BuildingModels getFk_building() {
+//        return fk;
+//    }
+//
+//    public void setFk_building(BuildingModels fk) {
+//        this.fk = fk;
+//    }
+//}
