eaad77cc23779475fe1dd6229849097bc3504320
Wei Yi Lim <2474554l@student.gla.ac.uk>
2021-03-03 16:34:48 +0800

    Working on #111, added dont destory load in timer, timer will continue even enter other scenes

--- Tower Building App/Assets/Scripts/UI/StopWatch.cs
+++ Tower Building App/Assets/Scripts/UI/StopWatch.cs
@@ -13,8 +13,25 @@ public class StopWatch : MonoBehaviour {
     //The variable will be used in scoring.cs 
     public static float TimeCounted;
     //Used lower case character to indicate private field
-    private bool playing;
-    
+    private static bool playing;
+    private DateTime gamePausedTime;
+    public GameObject PauseButton;
+    public GameObject PlayButton;
+    //The dont destory object is canvas, it must be a parentless gameobject 
+    public GameObject TimerDontDestory;
+    public static StopWatch Instance;
+
+    //Avoid multiple instances
+    void Awake(){
+        if (Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(TimerDontDestory);
+        }
+        else{
+            Destroy(TimerDontDestory);
+        }
+    }
+
 
     void Update () {
         if (playing == true)
@@ -25,11 +42,35 @@ public class StopWatch : MonoBehaviour {
             string seconds = (TimeCounted % 60).ToString("00");
             TimerText.text = hours + ":" + minutes + ":" + seconds;
         }
+        Debug.Log(gamePausedTime);
+        // Debug.Log((float)(DateTime.Now - gamePausedTime).TotalSeconds);
     }
+
+    // void OnApplicationPause (bool isGamePause)
+    // {   
+    //     //Store the current time when the app is paused
+    //     if (isGamePause) {
+    //         gamePausedTime = DateTime.Now;
+    //     }
+    // }
+
+    // void OnApplicationFocus  (bool isGameFocus)
+    // {
+    //     if (isGameFocus) {
+    //         BackgroundTimer();
+    //     }
+    //     // Debug.Log(TimeCounted);
+    // }
+
+    // void BackgroundTimer(){
+    //     TimeCounted += ((float)(DateTime.Now - gamePausedTime).TotalSeconds);
+    // }
 
     public void ClickPlay ()
     {
         playing = true;
+        PauseButton.SetActive(true);
+        PlayButton.SetActive(false);
     }
 
     public void ClickStop()
@@ -39,5 +80,7 @@ public class StopWatch : MonoBehaviour {
         TimeCounted = 0;
         //Replace the timer text to 00:00:00
         TimerText.text = "00:00:00";
+        PauseButton.SetActive(false);
+        PlayButton.SetActive(true);
     }
 }
