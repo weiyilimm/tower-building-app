7489d26314f7b6dbc2eaee60e5712c957d9b7c25
Vedant Rakesh Dube <dubevedant@gmail.com>
2021-03-10 17:13:44 +0530

    Working on #116. Removed Decoupled Users Repository

--- backend/src/main/java/com/example/towerbuilderspring/repository/UserModelRepositoryDecoupled.java
+++
@@ -1,9 +0,0 @@
-package com.example.towerbuilderspring.repository;
-
-import com.example.towerbuilderspring.model.UserModelDecoupled;
-import com.example.towerbuilderspring.model.UserModelIdDecoupled;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserModelRepositoryDecoupled extends JpaRepository<UserModelDecoupled, UserModelIdDecoupled> {
-
-}
