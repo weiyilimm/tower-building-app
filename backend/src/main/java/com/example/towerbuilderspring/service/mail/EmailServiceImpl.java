53d07f9365d666bf083e48898f74b47d692f281a
Vedant Rakesh Dube <dubevedant@gmail.com>
2021-03-12 12:39:44 +0530

    Working on #115. Created the email service skeleton

---
+++ backend/src/main/java/com/example/towerbuilderspring/service/mail/EmailServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.towerbuilderspring.service.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+
+public class EmailServiceImpl {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public EmailServiceImpl () {};
+
+    public void sendSimpleMessage(String to, String subject, String text) throws MailException {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("pseudolabsrobota@gmail.com");
+        message.setTo(to);
+
+        message.setSubject(subject);
+        message.setText(text);
+
+        System.out.println("Created email " + message.toString());
+
+        emailSender.send(message);
+    }
+}
