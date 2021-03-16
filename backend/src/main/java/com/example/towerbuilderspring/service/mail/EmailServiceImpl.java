cd616fc8805efd647af7a60299e9d1e380f254a4
Vedant Rakesh Dube <dubevedant@gmail.com>
2021-03-16 10:39:12 +0530

    Working on #115. Added function to handle MIME messages.

--- backend/src/main/java/com/example/towerbuilderspring/service/mail/EmailServiceImpl.java
+++ backend/src/main/java/com/example/towerbuilderspring/service/mail/EmailServiceImpl.java
@@ -1,11 +1,17 @@
 package com.example.towerbuilderspring.service.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Component
 public class EmailServiceImpl {
@@ -29,6 +35,23 @@ public class EmailServiceImpl {
         emailSender.send(message);
     }
 
+    public void sendMessageWithAttachment(String to,
+                                          String from,
+                                          String subject,
+                                          String pathToAttachment) throws MessagingException {
+
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
+        helper.addAttachment("Invoice", file);
+
+        emailSender.send(message);
+    }
 
 
 }
