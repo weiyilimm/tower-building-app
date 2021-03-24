0a0b0b85ea7bea1edbb74ced696b44bccd20ffcb
Wei Yi Lim <2474554l@student.gla.ac.uk>
2021-03-24 16:16:14 +0800

    Working on #111, fixed timer bug. Timer can now run on other scene as well as background

--- Tower Building App/Assets/Scripts/UI/StopWatch.cs
+++ Tower Building App/Assets/Scripts/UI/StopWatch.cs
@@ -11,25 +11,29 @@ public class StopWatch : MonoBehaviour {
     //Used for 00:00:00 text shown in the clock scene
     public TextMeshProUGUI TimerText;
     //The variable will be used in scoring.cs 
-    public static float TimeCounted;
+    private float TimeCounted;
     //Used lower case character to indicate private field
-    private static bool playing;
-    private DateTime gamePausedTime;
+    private bool playing;
     public GameObject PauseButton;
     public GameObject PlayButton;
 
     void Start(){
+        playing = Clock.playing;
+        
         if (playing == true)
         {
             PauseButton.SetActive(true);
             PlayButton.SetActive(false);
         }
+        else{
+            PauseButton.SetActive(false);
+            PlayButton.SetActive(true);
+        }
     }
     void Update () {
-        if (playing == true)
+        if (Clock.playing == true)
         {   
-            
-            TimeCounted += Time.deltaTime;
+            TimeCounted = Clock.TimeCounted;
             string hours = Mathf.Floor((TimeCounted % 216000) / 3600).ToString("00");
             string minutes = Mathf.Floor((TimeCounted % 3600) / 60).ToString("00");
             string seconds = (TimeCounted % 60).ToString("00");
@@ -37,46 +41,18 @@ public class StopWatch : MonoBehaviour {
         }
     }
 
-    
-    //Unity built-in method to detect when the user left the app
-    void OnApplicationPause (bool isGamePause)
-    {   
-        //Store the current time when the app is paused
-        if (isGamePause) {
-            gamePausedTime = DateTime.Now;
-        }
-    }
-
-    
-    //Unity built-in method to detect when the user is come back
-    void OnApplicationFocus  (bool isGameFocus)
-    {   
-        //Trigger BackgroundTimer method when user come back 
-        if (isGameFocus && playing == true ) {
-            BackgroundTimer();
-        }
-    }
-
-    /*
-    Calculate the total time difference between 
-    time when user left and time when user come back
-    */
-    void BackgroundTimer(){
-        TimeCounted += ((float)(DateTime.Now - gamePausedTime).TotalSeconds);
-    }
-
     public void ClickPlay ()
     {
-        playing = true;
+        Clock.playing = true;
         PauseButton.SetActive(true);
         PlayButton.SetActive(false);
     }
 
     public void ClickStop()
     {   
-        playing = false;
+        Clock.playing = false;
         //Set the time to 0 
-        TimeCounted = 0;
+        Clock.TimeCounted = 0;
         //Replace the timer text to 00:00:00
         TimerText.text = "00:00:00";
         PauseButton.SetActive(false);
