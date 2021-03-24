d1335369ec818e70a153c61936e05a50611cba84
weiyilimm <lwy_1011@hotmail.com>
2021-03-24 22:28:36 +0800

    Closes #125, created xp information button which created by Joe

---
+++ Tower Building App/Assets/Scripts/UI/xpInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class xpInfo : MonoBehaviour
+{   
+    public Button PanelButton;
+    public GameObject Panel;
+    public Button InfoButton;
+    void Start()
+    {
+        PanelButton.onClick.AddListener(() => HideInfo());
+        InfoButton.onClick.AddListener(() => ShowInfo());
+    }
+
+    void ShowInfo(){
+        Panel.SetActive(true);
+    }
+
+    void HideInfo(){
+        Panel.SetActive(false);
+    }
+
+}
