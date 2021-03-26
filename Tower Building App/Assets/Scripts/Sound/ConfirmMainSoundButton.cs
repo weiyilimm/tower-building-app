d61032962d40d580bf96262b5e7c2e9fbe91739a
Reuben Markoff <2383199m@student.gla.ac.uk>
2021-03-26 17:41:10 +0000

    working on issue #64 fixed issue for theme music not resetting to normal after editing main building

---
+++ Tower Building App/Assets/Scripts/Sound/ConfirmMainSoundButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmMainSoundButton : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(ConfirmSound);
+    }
+    public void ConfirmSound()
+    {
+        FindObjectOfType<SoundManager>().Play("standard button click");
+        //turning the filter off and setting its frequency
+        FindObjectOfType<ListenerPersist>().toggleFilterOn(false);
+        FindObjectOfType<ListenerPersist>().setFilterFrequency(500);
+    }
+}
