4fe96ca1debe8dd5df416771029fde59ea54e40f
Reuben Markoff <2383199m@student.gla.ac.uk>
2021-03-26 17:17:33 +0000

    working on issue #64 fixed some sound bugs that had gone unnoticed, and attached sound to the major buttons that didn't have any

---
+++ Tower Building App/Assets/Scripts/Sound/StandardButtonClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StandardButtonClick : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(StandardButtonClickSound);
+    }
+    public void StandardButtonClickSound()
+    {
+        FindObjectOfType<SoundManager>().Play("standard button click");
+    }
+}
