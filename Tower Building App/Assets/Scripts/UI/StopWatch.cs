a29dff38035edc2b98ec78a0f1ff458c9bf7148e
Wei Yi Lim <2474554l@student.gla.ac.uk>
2021-03-27 15:43:21 +0800

    Implemented lock feature for drop down, when the timer starts the drop down get locked

--- Tower Building App/Assets/Scripts/UI/StopWatch.cs
+++ Tower Building App/Assets/Scripts/UI/StopWatch.cs
@@ -16,12 +16,14 @@ public class StopWatch : MonoBehaviour {
     private bool playing;
     public GameObject PauseButton;
     public GameObject PlayButton;
-
+    public TMP_Dropdown DropDown;
+    public static string selectedSubject;
     void Start(){
         playing = Clock.playing;
         
         if (playing == true)
-        {
+        {   
+            DropDown.options[DropDown.value].text = selectedSubject;
             PauseButton.SetActive(true);
             PlayButton.SetActive(false);
         }
@@ -33,11 +35,16 @@ public class StopWatch : MonoBehaviour {
     void Update () {
         if (Clock.playing == true)
         {   
+            DropDown.interactable = false; 
+            selectedSubject = DropDown.options[DropDown.value].text;
             TimeCounted = Clock.TimeCounted;
             string hours = Mathf.Floor((TimeCounted % 216000) / 3600).ToString("00");
             string minutes = Mathf.Floor((TimeCounted % 3600) / 60).ToString("00");
             string seconds = (TimeCounted % 60).ToString("00");
             TimerText.text = hours + ":" + minutes + ":" + seconds;
+        }
+        else{
+            DropDown.interactable = true; 
         }
     }
 
